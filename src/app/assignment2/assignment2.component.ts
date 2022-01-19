d313293bac95b1b207527f00d3cdecab10694a07
avneesh <singhtest18@gmail.com>
2022-01-19 08:17:15 +0000

    Completed assignment 2, added missed files

---
+++ src/app/assignment2/assignment2.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component implements OnInit {
+
+  username: string ='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clearInputField() {
+    this.username = '';
+  }
+
+  inputEventTest(event: Event) {
+    console.info(event);
+  }
+
+}
